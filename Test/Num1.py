d2c3256b84e0d5a54a5b832248a7a30b86ac2a7e
AlphaAE <AlphaAE@SURFACE-ALPHAAE>
2020-10-14 08:46:39 +0800

    20201014

--- Test/Num1.py
+++ Test/Num1.py
@@ -1,20 +1,18 @@
+import math
 
 arr = [171,162,155,184,445,362,351,689,772]
 
 # 均值
-sumNumTemp = 0
-for i in arr:
-    sumNumTemp += i
-avgNum = sumNumTemp / len(arr)
+avgNum = sum(arr) / len(arr)
 print("均值：{:.3f}".format(avgNum))
 
 # 方差
 varianceArr = []
 for i in arr:
     varianceArr.append((i - avgNum) ** 2)
-print(varianceArr)
-
-
-
+variance = sum(varianceArr) / len(varianceArr)
+print("方差：{:.3f}".format(variance))
 
 # 标准差
+standardDeviation = math.sqrt(variance)
+print("标准差：{:.3f}".format(standardDeviation))
