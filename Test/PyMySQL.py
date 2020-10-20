f9616b53cd25109bebc762f71686eff8f3a4e233
AlphaAE <AlphaAE@SURFACE-ALPHAAE>
2020-10-20 17:36:58 +0800

    20201020_3

--- Test/PyMySQL.py
+++ Test/PyMySQL.py
@@ -4,11 +4,12 @@ import pymysql as mysql
 db = mysql.connect("db.alphaae.com", "testuser", "123456", "TESTDB")
 
 cursor = db.cursor()
-
 cursor.execute("SELECT VERSION()")
-
 date = cursor.fetchone()
-
 print("DB version: {}".format(date))
+
+cursor.execute("SELECT * FROM student")
+date = cursor.fetchall()
+print("Data: {}".format(date))
 
 db.close()
