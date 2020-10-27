f662f3ea7cb9a4c73ace7cca98eddc89afbe9431
alphaae <a1226123914@gmail.com>
2020-10-27 21:09:37 +0800

    20201027_1

--- Test/test20201022.py
+++ Test/test20201022.py
@@ -1,21 +1,38 @@
-### 
 import random
-numList = list(range(100))
+import time
 
-luckNum = random.choice(numList)
-random.shuffle(numList)
+### 1
+# numList = list(range(100))
 
-for i in numList:
-    if i == luckNum:
-        print("恭喜，号码[{}]中奖了！".format(i))
-        break
-    else:
-        print("抱歉，号码[{}]没有中奖。".format(i))
+# luckNum = random.choice(numList)
+# random.shuffle(numList)
+
+# for i in numList:
+#     if i == luckNum:
+#         print("恭喜，号码[{}]中奖了！".format(i))
+#         break
+#     else:
+#         print("抱歉，号码[{}]没有中奖。".format(i))
 
 ### 冒泡
-numList = list(range(100))
+numList = list(range(20))
 random.shuffle(numList)
+print(numList)
 
+for i in range(len(numList) - 1):
+    for j in range(0, len(numList) - 1 - i):
+        if numList[j] > numList[j + 1]:
+            tmp = numList[j]
+            numList[j] = numList[j + 1]
+            numList[j + 1] = tmp
+
+print(numList)
 
 ### 进度条 20 后低速 到 30 - 100 加速
-
+sleepTime = 1.0
+for i in range(101):
+    if i % 20 == 0:
+        sleepTime = random.choice(range(0, int(sleepTime * 100.0))) / 100.0
+    print("\r [{}{}]  {}% sleepTime:{}".format(">" * i, "-" * (100 - i), i, sleepTime), end="")
+    time.sleep(sleepTime)
+print()
