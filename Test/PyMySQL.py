619fc8bf4809e10991e8c9c505504dc85dc85789
AlphaAE <AlphaAE@SURFACE-ALPHAAE>
2020-11-10 16:40:54 +0800

    20201110_1

--- Test/PyMySQL.py
+++ Test/PyMySQL.py
@@ -4,12 +4,13 @@ import pymysql as mysql
 db = mysql.connect("db.alphaae.com", "testuser", "123456", "TESTDB")
 
 cursor = db.cursor()
-cursor.execute("SELECT VERSION()")
-date = cursor.fetchone()
-print("DB version: {}".format(date))
+# cursor.execute("SELECT VERSION()")
+# date = cursor.fetchone()
+# print("DB version: {}".format(date))
 
-cursor.execute("SELECT * FROM student")
-date = cursor.fetchall()
-print("Data: {}".format(date))
+for i in range(1000000):
+    cursor.execute("INSERT INTO student(`name`,sex) VALUES ('李华{}', '1'),('梨花{}', '0')".format(i, i))
+    db.commit()
+    print("Data: {}".format(i))
 
 db.close()
---
+++ Test/test20201104.py
@@ -0,0 +1,5 @@
+# File IO
+
+tmpFile = open("./Test/PyMySQL.py", "r+", True)
+
+print(tmpFile.read())
