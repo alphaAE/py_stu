113b3351d92931ae984e566d7fb5d334864122bf
AlphaAE <AlphaAE@SURFACE-ALPHAAE>
2020-12-09 11:58:49 +0800

    1209

---
+++ Test/test20201203.py
@@ -0,0 +1,33 @@
+#  生成数据
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame()
+index = ["alpha", "beta", "gamma", "delta", "eta"]
+for i in range(5):
+    a = pd.DataFrame([np.linspace(i, 5 * i, 5)], index=[index[i]])
+    df = pd.concat([df, a], axis=0)
+
+print(df, "\n")
+
+# 获取列数据
+print(df[[1, 2]], "\n")
+
+# 修改列名
+df.columns = ["a", "b", "c", "d", "e"]
+print(df, "\n")
+
+# 获取第二列数据
+print(df["b"], "\n")
+# print(df.b, "\n")
+# print(df.iloc[:, 1], "\n")
+# print(df.loc[:, "b"], "\n")
+
+# # 取出 a,d列数据
+print(df[["a", "d"]], "\n")
+
+# # 获取 c列下的beta值
+print(df["c"]["beta"], "\n")    # 显式
+print(df["c"][1], "\n")         # 隐式
+print(df.iloc[1, 2], "\n")
+print(df.loc["beta", "c"], "\n")
---
+++ Test/test20201209.py
@@ -0,0 +1,39 @@
+import pandas
+import numpy
+
+
+index = ("Java", "Python", "MySQL", "Linux")
+numpy.random.seed(0)
+dic1 = {
+    "张三": numpy.random.randint(120, 150, 4),
+    "李四": numpy.random.randint(120, 150, 4),
+    "王五": numpy.random.randint(120, 150, 4),
+    "赵六": numpy.random.randint(120, 150, 4)
+}
+df1 = pandas.DataFrame(dic1, index)
+numpy.random.seed(1)
+dic2 = {
+    "张三": numpy.random.randint(120, 150, 4),
+    "李四": numpy.random.randint(120, 150, 4),
+    "王五": numpy.random.randint(120, 150, 4),
+    "赵六": numpy.random.randint(120, 150, 4)
+}
+df2 = pandas.DataFrame(dic2, index)
+
+# 期中与期末的成绩和
+print(df1 + df2, "\n")
+
+# 期中与期末的平均值
+print((df1 + df2) * 0.5, "\n")
+
+# 张三期中Python记0
+df1.at["Python", "张三"] = 0
+print(df1, "\n")
+
+# 李四期中所有加10
+df1.loc[:, "李四"] += 10
+print(df1, "\n")
+
+# 所有Python成绩加15
+df1.loc["Python", :] += 15
+print(df1, "\n")
